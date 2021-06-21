2e0aa7f1ba71903576b1814d44b1002fafaf34bb
Daine Bigham <daine.bigham@gmail.com>
2021-06-21 19:09:43 +1200

    updated to use json instead of plaintext

--- window.py
+++ window.py
@@ -3,7 +3,7 @@ from tkinter import messagebox
 import string
 import random
 import pyperclip
-
+import json
 class Window(Tk):
     def __init__(self):
         super().__init__()
@@ -25,8 +25,8 @@ class Window(Tk):
         lbl_password = Label(text='Password :')
         lbl_password.grid(row=3, column=0)
 
-        self.input_website = Entry(width=45,)
-        self.input_website.grid(row=1, column=1, columnspan=2, pady=2)
+        self.input_website = Entry(width=34)
+        self.input_website.grid(row=1, column=1, pady=2)
         self.input_website.focus()
         self.input_username = Entry(width=45)
         self.input_username.insert(0, 'daine.bigham@gmail.com')
@@ -34,10 +34,12 @@ class Window(Tk):
         self.input_password = Entry(width=34)
         self.input_password.grid(row=3, column=1, pady=2)
 
-        self.btn_password = Button(text=' Generate ', command=self.generate_password)
+        self.btn_password = Button(text='Generate', width=8, command=self.generate_password)
         self.btn_password.grid(row=3, column=2, pady=2)
         self.btn_add = Button(text='Add', width=38, command=self.save)
         self.btn_add.grid(row=4, column=1, columnspan=2, pady=2)
+        self.btn_search = Button(text='Search', width=8)
+        self.btn_search.grid(row=1, column=2, pady=2)
 
         self.mainloop()
     
@@ -47,17 +49,25 @@ class Window(Tk):
         username = self.input_username.get()
         password = self.input_password.get()
 
+        new_data = {
+            website : {
+                'email' : username,
+                'password' : password
+            }
+        }
+
         if len(website) == 0 or len(password) == 0: 
             messagebox.showinfo(title='Oops...', message="Looks like you're missing something")
         else: 
-            is_ok = messagebox.askokcancel(title=website, message=f'Are you sure?\nUsername: {username}\nPassword: {password}')
+            with open('data.json', 'r') as f:
+                data = json.load(f)
+                data.update(new_data)
+                
+            with open('data.json', 'w') as f:
+                json.dump(data, f, indent=4)
 
-            if is_ok:
-                with open('data.txt', 'a') as f:
-                    f.write(f'{website} | {username} | {password}\n')
-
-                    self.input_website.delete(0, END)
-                    self.input_password.delete(0, END)
+                self.input_website.delete(0, END)
+                self.input_password.delete(0, END)
 
     def generate_password(self):
         characters = string.ascii_letters + string.digits + string.punctuation
